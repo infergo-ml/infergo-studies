17758a4f67ad67de020956d458b718c3b572cc42
David Tolpin <david.tolpin@gmail.com>
2019-06-17 08:28:27 +0300

    cleaned up gmm, bumped up to 6.3

--- gmm/model/model.go
+++ gmm/model/model.go
@@ -44,12 +44,8 @@ func (m *Model) Observe(x []float64) float64 {
 
 	// Fetch observation probabilities.
 	p := make([][]float64, len(m.Data))
-	theta := make([][]float64, len(m.Data))
 	for i := range m.Data {
 		p[i] = make([]float64, m.NComp)
-		theta[i] = make([]float64, m.NComp)
-
-		theta[i] = x[ix : ix+m.NComp]
 		D.SoftMax(x[ix:ix+m.NComp], p[i])
 		// Observe them from the Dirichlet to adjust the
 		// contrast.
@@ -62,8 +58,7 @@ func (m *Model) Observe(x []float64) float64 {
 		var l float64
 		for j := 0; j != m.NComp; j++ {
 			lj := Normal.Logp(mu[j], sigma[j], m.Data[i]) +
-				theta[i][j]
-				// math.Log(p[i][j])
+				 math.Log(p[i][j])
 			if j == 0 {
 				l = lj
 			} else {
--- lr/go.mod
+++ lr/go.mod
@@ -1,3 +1,3 @@
 module bitbucket.org/dtolpin/infergo-studies/lr
 
-require bitbucket.org/dtolpin/infergo v0.6.2
+require bitbucket.org/dtolpin/infergo v0.6.3
